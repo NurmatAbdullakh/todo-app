25d3e771cc1c0ff6e72f35074db00624dbb183e5
Nurmat Abdullaev <nurmat2396@gmail.com>
2022-12-21 17:05:11 +0600

    clear and done buttons are ready

--- script/script.js
+++ script/script.js
@@ -1,24 +1,32 @@
 const tasks=document.querySelector(".tasks")
 const addButton=document.querySelector(".addButton")
 const input=document.querySelector(".input")
+const clear=document.querySelector(".clear")
 let tasksList=[]
-
 
 const Rerendering=()=>{
     tasks.innerHTML=''
+    
     tasksList.forEach((item,index)=>{
+        const textClassName=item.isDone ? "text done" : "text"
         tasks.innerHTML+=`<li class="item">
-        <span class="text ">${item.task}</span>
+        <span class="${textClassName}">${item.task}</span>
         <div class="buttons">
-            <img class="done" src="./images/Vector.svg" alt="">
+            <img class="done" onclick="doneClick(\'${item.task}\')" src="./images/Vector.svg" alt="">
             <img class="delete" onclick="deleteClick(\'${item.task}\')" src="./images/↳ Icon Color.svg" alt="">
         </div>
     </li> `
     })
 }
+
 addButton.addEventListener("click",()=>{
     const value=input.value
-    tasksList.push({task:value})
+    if(value.trim()){
+        tasksList.unshift({task:value,isDone:false})
+        input.value=""
+    }else{
+        alert("Please enter anything")
+    }
     Rerendering()
 })
 
@@ -27,3 +35,12 @@ function deleteClick(task){
     tasksList.splice(currentIndex,1)
     Rerendering()
 }
+function doneClick(task){
+    const currentIndex=tasksList.findIndex(v=>v.task===task)
+    tasksList[currentIndex].isDone=true
+    Rerendering()
+}
+clear.addEventListener("click",()=>{
+    tasksList=[]
+    Rerendering()
+})
